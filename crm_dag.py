23c7837f54643f89842c971bbe9adb8a32b3aab4
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-11-01 14:49:31 +0300

    v0

---
+++ crm_dag.py
@@ -0,0 +1,63 @@
+import datetime as dt
+from dateutil.relativedelta import relativedelta
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.task_group import TaskGroup
+from airflow.operators.dummy import DummyOperator
+from airflow.contrib.operators.vertica_operator import VerticaOperator
+
+from crm_etl.scripts.collable import etl
+
+
+default_args = {
+    'owner': 'Швейников Андрей',
+    'email': ['xxxRichiexxx@yandex.ru'],
+    'retries': 3,
+    'retry_delay': dt.timedelta(minutes=30),
+}
+with DAG(
+        'crm_dag',
+        default_args=default_args,
+        description='Получение данных из CRM.',
+        start_date=dt.datetime(2022, 1, 1),
+        schedule_interval='@monthly',
+        catchup=True,
+        max_active_runs=1
+) as dag:
+
+    start = DummyOperator(task_id='Начало')
+
+    with TaskGroup('Загрузка_данных_в_stage_слой') as data_to_stage:
+
+        tasks = []
+
+        for offset in range(0, 2):
+            tasks.append(
+                PythonOperator(
+                    task_id=f'get_requests_offset_{offset}',
+                    python_callable=etl,
+                    op_kwargs={
+                        'offset': offset,
+                        'data_type': 'stage_crm_requests',
+                    },
+                )
+            )
+
+        tasks
+
+    with TaskGroup('Загрузка_данных_в_dds_слой') as data_to_dds:
+
+        pass
+
+    with TaskGroup('Загрузка_данных_в_dm_слой') as data_to_dm:
+
+        pass
+
+    with TaskGroup('Проверки') as data_checks:
+
+        pass
+
+    end = DummyOperator(task_id='Конец')
+
+    start >> data_to_stage >> data_to_dds >> data_to_dm >> data_checks >> end
---
+++ scripts/collable.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import glob
+import os
+import sys
+import sqlalchemy as sa
+from urllib.parse import quote
+from airflow.hooks.base import BaseHook
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+CODE_DIR_PATH = os.path.join(BASE_DIR, 'lib')
+sys.path.append(CODE_DIR_PATH)
+
+from crm import CRMExtractor
+
+
+def extract(source_url, source_username, source_password, execution_date, path):
+    """Извлечение данных из источника."""
+
+    print('ИЗВЛЕЧЕНИЕ ДАННЫХ')
+
+    extr = CRMExtractor(
+        source_username, 
+        source_password,
+        source_url,
+        path,
+        execution_date,
+        execution_date,
+    )
+
+    extr.get_requests()
+
+    result = pd.read_excel(
+        fr'{extr.file}',
+        sheet_name='Отчет',
+        header=11,
+        dtype=str,
+    )
+
+    # очищаем целевую папку
+    file_pattern = os.path.join(path, extr.file_pattern)
+    matching_files = glob.glob(file_pattern)
+    if matching_files:
+        for file_path in matching_files:
+            os.remove(file_path)
+            print(f"Датафрейм создан, файл экселя удален: {file_path}")
+    else:
+        print(f"Файлы, соответствующие шаблону имени 'Obracsheniya*', не найдены для загрузки.")
+
+    return result
+
+
+def transform(data, execution_date, data_type):
+    """Преобразование/трансформация данных."""
+
+    print('ТРАНСФОРМАЦИЯ ДАННЫХ')
+    print(data)
+    
+    if data_type == 'stage_crm_requests':
+        data.columns = [
+            "Skorost",
+            "Region",
+            'Holding',
+            'NomerObrashenia',
+            'KodDC',
+            'Dealer',
+            'DataSozdania',
+            'DataSmeniStatusa',
+            'Status',
+            'Comments',
+            'Prichina',
+            'IstochnicTrafica',
+            'TipZaiavki',
+            'Domen',
+            'IPAdresClienta',
+            'NomerRL',
+            'ModelInteresuiushegoTS',
+            'ClientID',
+            'Client',
+            'Phone',
+            'Email',
+            'TextObrashenia',
+            'SoglasieNaObrabotcuPD',
+            'SoglasieNaPoluchenieInformacii',
+            'ID',
+            'ObrashenieZakrito',
+            'TegiClienta',
+            'StatusRL',
+            'EtapProdaz',
+            'OblastClienta',
+            'ObsheeVremiaZvonca',
+            'StatusSviazannogoSobitiya',
+            'TypeOcherednogoSobitiya',
+            'DataOcherednogoSobitiya',
+            'OtvetstvenniyZaRL',
+        ]
+
+    data['period'] = execution_date
+    return data
+
+
+def load(dwh_engine, data, data_type, execution_date):
+    """Загрузка данных в хранилище."""
+
+    print('ЗАГРУЗКА ДАННЫХ')
+    if not data.empty:
+
+        print(data)
+
+        command = f"""
+            SELECT DROP_PARTITIONS(
+                'sttgaz.{data_type}',
+                '{execution_date.replace(day=1)}',
+                '{execution_date.replace(day=1)}'
+            );
+        """
+        print(command)
+
+        dwh_engine.execute(command)
+
+        data.to_sql(
+            f'{data_type}',
+            dwh_engine,
+            schema='sttgaz',
+            if_exists='append',
+            index=False,
+        )
+    else:
+        print('Нет новых данных для загрузки.')
+
+
+def etl(data_type, offset=None, **context):
+    """Запускаем ETL-процесс для заданного типа данных."""
+
+    source_con = BaseHook.get_connection('crm')
+    source_username = source_con.login
+    source_password = quote(source_con.password)
+    source_url = source_con.host
+
+    dwh_con = BaseHook.get_connection('vertica')
+    ps = quote(dwh_con.password)
+    dwh_engine = sa.create_engine(
+        f'vertica+vertica_python://{dwh_con.login}:{ps}@{dwh_con.host}:{dwh_con.port}/sttgaz'
+    )
+
+    if offset:
+        month = context['execution_date'].month - offset
+        if month <= 0:
+            month = 12 + month
+            execution_date = context['execution_date'].date().replace(month = month, year = context['execution_date'].year - 1, day=1)
+        else:
+            execution_date = context['execution_date'].date().replace(month = month, day=1)
+    else:
+        execution_date = context['execution_date'].date().replace(day=1)
+
+    path = f"C:\\tmp\{execution_date}"
+
+    data = extract(source_url, source_username, source_password, execution_date, path)
+    data = transform(data, execution_date, data_type)
+    load(dwh_engine, data, data_type, execution_date)
