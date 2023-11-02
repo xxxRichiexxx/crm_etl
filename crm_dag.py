80de8a9622653f58b346b6be89a6cd4a63fc95f7
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-11-02 16:57:10 +0300

    v0

--- crm_dag.py
+++ crm_dag.py
@@ -20,7 +20,7 @@ with DAG(
         default_args=default_args,
         description='Получение данных из CRM.',
         start_date=dt.datetime(2022, 1, 1),
-        schedule_interval='@monthly',
+        schedule_interval='@daily',
         catchup=True,
         max_active_runs=1
 ) as dag:
