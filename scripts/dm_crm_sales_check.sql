46a45ad74dda8a679cd11ccb5f43061e47071528
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2023-11-20 08:34:43 +0300

    check change

--- scripts/dm_crm_sales_check.sql
+++ scripts/dm_crm_sales_check.sql
@@ -1,11 +1,11 @@
 INSERT INTO sttgaz.stage_checks (table_name, check_name, ts, check_result)
 WITH sq AS(
-    SELECT SUM(Реализовано)
+    SELECT SUM(Продано)
     FROM sttgaz.dm_crm_sales s 
     WHERE s.period = '2023-02-01'
 )
 SELECT 
     '{{params.dm}}',
-    'comparison_with_target:' || ' 1813=' || (SELECT * FROM sq),
+    'comparison_with_target:' || ' 1813=?',
     NOW(),
     1813 = (SELECT * FROM sq);
